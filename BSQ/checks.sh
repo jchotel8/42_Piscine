cef3ffe92ab2377594444cc8b631a62ca41b8b88
jchotel8 <julchotel@gmail.com>
2020-02-20 16:58:15 +0100

    Removing all files for global update

--- BSQ/checks.sh
+++
@@ -1,6 +0,0 @@
-norminette -R CheckForbiddenSourceHeader | grep -e '^(Error|Warning)'
-norminette | grep -e '^(Error|Warning)'
-cat ./*/*.c | grep printf
-cat ./*/*.c | grep stdio
-cat ./*/*.c | grep "for"
-cat ./*/*.c | grep "do"
--- BSQ/test.sh
+++
@@ -1,2 +0,0 @@
-perl generator.pl 30 15 1 > test
-./bsq test
